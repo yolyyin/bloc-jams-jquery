6ac745b930ecda7eb626a7a19642742047e0f191
Tingyun Yin <tingyun_yin@example.com>
2020-04-30 02:11:55 -0400

    checkpoint completed

---
+++ scripts/song-list.js
@@ -0,0 +1,32 @@
+/* for_each album.songs as song
+song_element=
+  <tr>
+    <td>index</td>
+    <td>song.title</td>
+    <td>song.duration</td>
+  </tr>
+append song_element to #song-list
+end for_each */
+{
+  album.songs.forEach((song, index) => {
+    song.element = $(`
+      <tr>
+        <td>
+          <button>
+            <span class='song-number'>${index +1}</span>
+            <span class="ion-play"></span>
+            <span class="ion-pause"></span>
+          </button>
+        </td>
+        <td>${song.title}</td>
+        <td>${song.duration}</td>
+      </tr>
+    `);
+
+    song.element.on('click', event => {
+      player.playPause(song);
+    })
+
+    $('#song-list').append(song.element);
+  });
+}
