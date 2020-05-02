9916b75b0bc9c44bd59d29a55c8a9c85e2c19624
Tingyun Yin <tingyun_yin@example.com>
2020-05-02 11:33:23 -0400

    assignment completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,9 +10,20 @@
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if(nextSongIndex >= album.songs.length){return;}
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+  });
+
+  $('button#previous').on('click',function(){
+    if(player.playState !== 'playing'){return;}
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex-1;
+    if(previousSongIndex<0){return;}
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
   });
 
 }
